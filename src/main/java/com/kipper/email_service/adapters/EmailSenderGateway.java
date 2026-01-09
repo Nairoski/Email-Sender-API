9d374273b1d96976793447946b977b4e6a036758
Nairoski <pedro.nairoski@gmail.com>
2026-01-09 19:24:17 -0300

    First commit of Email API

---
+++ src/main/java/com/kipper/email_service/adapters/EmailSenderGateway.java
@@ -0,0 +1,5 @@
+package com.kipper.email_service.adapters;
+
+public interface EmailSenderGateway {
+	void sendEmail(String to, String subject, String body);
+}
---
+++ src/main/java/com/kipper/email_service/application/EmailSenderService.java
@@ -0,0 +1,25 @@
+package com.kipper.email_service.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kipper.email_service.adapters.EmailSenderGateway;
+import com.kipper.email_service.core.EmailSenderUseCase;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+	
+	private final EmailSenderGateway emailSenderGateway;
+	
+	@Autowired
+	public EmailSenderService(EmailSenderGateway emailGateway) {
+		this.emailSenderGateway = emailGateway;
+	}
+
+	@Override
+	public void sendEmail(String to, String subject, String body) {
+		this.emailSenderGateway.sendEmail(to, subject, body);
+		
+	}
+
+}
